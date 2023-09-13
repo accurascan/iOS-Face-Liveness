e7d9fe52b6f6e18b138520833c87dc2e4652a423
Anas <anas@accurascan.com>
2023-09-13 14:25:35 +0530

    Initial Commit

---
+++ AccuraLiveness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccuraLiveness
+//
+//  Created by Mac on 13/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
